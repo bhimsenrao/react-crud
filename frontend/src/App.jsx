7d89c75e37fcce49f1335fec30ee97d379c9878a
bhimsen <brkaran2k2@gmail.com>
2026-09-06 17:16:34 +0530

    App.jsx updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-
 import AccountsCRUD3 from './components/AccountsCRUD3'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <AccountsCRUD3 />
+ <AccountsCRUD3 />
     </>
   );
 }
